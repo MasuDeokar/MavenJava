3d4ec06f422484eccace6f54c5dc47cbbc988a86
MasuDeokar <mayureshdeokar@gmail.com>
2024-02-07 15:58:33 +0530

    first commit

---
+++ src/test/java/masudeomvn/Mavenjava/Rest.java
@@ -0,0 +1,17 @@
+package masudeomvn.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class Rest {
+
+	@Test
+	public void postJira()
+	{
+		System.out.println("postJira");
+	}
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+}
